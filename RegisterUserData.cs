1fba15202d7c8968aabf679688152478058706a2
karlopenguini <karlobpalisoc@gmail.com>
2022-06-06 13:41:13 +0800

    Add project files.

---
+++ RegisterUserData.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CaloRead.Activities.RegistrationActivities
+{
+    [Activity(Label = "Register1")]
+    public class RegisterUserData : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
