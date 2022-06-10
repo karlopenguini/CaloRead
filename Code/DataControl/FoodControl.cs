a3feb26ca814294e663441cdf800b73481b32f85
Stephen Tongol <spvtongol@gmail.com>
2022-06-10 15:35:15 +0800

    Changes

--- Code/DataControl/FoodControl.cs
+++ Code/DataControl/FoodControl.cs
@@ -32,5 +32,38 @@ namespace CaloRead
                 return false;
             }
         }
+
+        public static bool Remove(float kcal, float protein, float carbs, float fats, string name, float grams)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"http://192.168.1.6/caloread/removefood.php?kcal={kcal}&protein={protein}&carbs={carbs}&fats={fats}&name={name}&grams={grams}");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            String res = reader.ReadToEnd();
+            if (res.Contains("Data Removed"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool Edit(float kcal, float protein, float carbs, float fats, string name, float grams)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"http://192.168.1.6/caloread/updatefood.php?kcal={kcal}&protein={protein}&carbs={carbs}&fats={fats}&name={name}&grams={grams}");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            String res = reader.ReadToEnd();
+            if (res.Contains("Data Updated"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
--- Code/Main/Food/EditFood.cs
+++ Code/Main/Food/EditFood.cs
@@ -14,6 +14,10 @@ namespace CaloRead
 {
     public class EditFood : AndroidX.Fragment.App.Fragment
     {
+        ImageButton goBack;
+        EditText kcal, protein, carbs, fats, name, grams;
+        Button update, remove;
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -25,11 +29,36 @@ namespace CaloRead
         {
             // Use this to return your custom view for this Fragment
             // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
-            var activty = Activity as App;
-            activty.FindViewById<LinearLayout>(Resource.Id.header).Visibility = ViewStates.Visible;
-            activty.FindViewById<ImageButton>(Resource.Id.BTN_Calendar).Visibility = ViewStates.Gone;
+            var activity = Activity as App;
+            activity.FindViewById<LinearLayout>(Resource.Id.header).Visibility = ViewStates.Visible;
+            activity.FindViewById<ImageButton>(Resource.Id.BTN_Calendar).Visibility = ViewStates.Gone;
 
             View view = inflater.Inflate(Resource.Layout.EditFood, container, false);
+
+            // Instantiate Widgets
+            goBack = view.FindViewById<ImageButton>(Resource.Id.BTN_GoBack_EditFood);
+            kcal = view.FindViewById<EditText>(Resource.Id.ET_kcal_EditFood);
+            protein = view.FindViewById<EditText>(Resource.Id.ET_Protein_EditFood);
+            carbs = view.FindViewById<EditText>(Resource.Id.ET_Carbs_EditFood);
+            fats = view.FindViewById<EditText>(Resource.Id.ET_Fats_EditFood);
+            name = view.FindViewById<EditText>(Resource.Id.ET_FoodName_EditFood);
+            grams = view.FindViewById<EditText>(Resource.Id.ET_Grams_EditFood);
+            update = view.FindViewById<Button>(Resource.Id.BTN_Update_EditFood);
+            remove = view.FindViewById<Button>(Resource.Id.BTN_Remove_EditFood);
+
+            update.Click += (s, e) =>
+            {
+                if (FoodControl.Edit(float.Parse(kcal.Text), float.Parse(protein.Text), float.Parse(carbs.Text), float.Parse(fats.Text), name.Text, float.Parse(grams.Text)))
+                {
+                    activity.ShowMessage("Food Updated!");
+                    activity.ChangeFragment(activity.food);
+                }
+                else
+                {
+                    activity.ShowMessage("Unable to Update Food!");
+                }
+            };
+
             return view;
         }
     }
