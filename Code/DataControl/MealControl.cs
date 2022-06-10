d3100c170febe9f6a44ab4f2304213317b393529
karlopenguini <karlobpalisoc@gmail.com>
2022-06-10 20:00:57 +0800

    asdasd

--- Code/DataControl/MealControl.cs
+++ Code/DataControl/MealControl.cs
@@ -31,6 +31,7 @@ namespace CaloRead
             var meals = root;
 
             List<Meal.MealItem> mealitems = new List<Meal.MealItem>();
+            List<int> foodIDs = new List<int>();
             foreach(JsonElement meal in root.EnumerateArray())
             {
                 string date = meal.GetProperty("date").ToString();
@@ -39,35 +40,30 @@ namespace CaloRead
                 float servings = float.Parse(meal.GetProperty("servings").ToString());
                 string _type = meal.GetProperty("type").ToString();
 
+                foodIDs.Add(foodID);
 
-                var foodDoc = GetFood(foodID);
-                JsonElement foodRoot = foodDoc.RootElement;
-                var food = foodRoot[0];
+                //request = (HttpWebRequest)WebRequest.Create($"http://192.168.1.6/caloread/searchfood.php?foodID={foodID}");
+                //response = (HttpWebResponse)request.GetResponse();
+                //res = response.ProtocolVersion.ToString();
+                //reader = new StreamReader(response.GetResponseStream());
+                //result = reader.ReadToEnd();
+                //using JsonDocument docFood = JsonDocument.Parse(result);
+                //JsonElement rootFood = docFood.RootElement;
 
-                string foodname = food.GetProperty("foodname").ToString();
-                float calories = float.Parse(food.GetProperty("calories").ToString()) * servings;
-                float totalgrams = float.Parse(food.GetProperty("grams").ToString()) * servings;
-                float protein = float.Parse(food.GetProperty("protein").ToString()) * servings;
-                float carbs = float.Parse(food.GetProperty("carbs").ToString()) * servings;
-                float fat = float.Parse(food.GetProperty("fat").ToString()) * servings;
+                //var food = rootFood[0];
+
+                //string foodname = food.GetProperty("foodname").ToString();
+                //float calories = float.Parse(food.GetProperty("calories").ToString()) * servings;
+                //float totalgrams = float.Parse(food.GetProperty("grams").ToString()) * servings;
+                //float protein = float.Parse(food.GetProperty("protein").ToString()) * servings;
+                //float carbs = float.Parse(food.GetProperty("carbs").ToString()) * servings;
+                //float fat = float.Parse(food.GetProperty("fat").ToString()) * servings;
 
 
-                mealitems.Add(new Meal.MealItem(mealID, foodname, calories, totalgrams, protein, carbs, fat));
+                //mealitems.Add(new Meal.MealItem(mealID, foodname, calories, totalgrams, protein, carbs, fat));
             }
 
             return mealitems.ToArray();
-        }
-
-        private JsonDocument GetFood(int FoodID)
-        {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"http://192.168.1.6/caloread/searchfood.php?foodID={FoodID}");
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            String res = response.ProtocolVersion.ToString();
-            StreamReader reader = new StreamReader(response.GetResponseStream());
-            var result = reader.ReadToEnd();
-            JsonDocument doc = JsonDocument.Parse(result);
-
-            return doc;
         }
 
         private class MealData
